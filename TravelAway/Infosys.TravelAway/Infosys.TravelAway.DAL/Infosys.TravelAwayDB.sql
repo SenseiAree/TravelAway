54a46e379a19d817a29b00934fd755616275c364
Vishal Shukla <vishal.shukla03@infosys.com>
2022-10-24 12:50:23 +0530

    SQL- CustomerTable Created

---
+++ TravelAway/Infosys.TravelAway/Infosys.TravelAway.DAL/Infosys.TravelAwayDB.sql
@@ -0,0 +1,36 @@
+﻿--DROP Scripts
+
+USE [master]
+GO
+IF (EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE ('[' + name + ']' = N'TravelAwayDB'OR name = N'TravelAwayDB')))
+DROP DATABASE TravelAwayDB
+
+CREATE DATABASE TravelAwayDB
+GO
+
+USE TravelAwayDB
+GO
+
+
+IF OBJECT_ID('Customers') IS NOT NULL
+	DROP TABLE Customers
+GO
+
+Create table Customers(
+	CustomerID int primary key,
+	FirstName varchar(30) not null,
+	LastName varchar(30) not null,
+	EmailId VARCHAR(50) CONSTRAINT [chk_EmailId] CHECK ([EmailId] LIKE '%_@__%.__%') NOT NULL,
+	Password varchar(16) not null,
+	ConfirmPassword varchar(16) not null,
+	Gender char(1) not null,
+    ContactNumber varchar(10) not null,
+    DateOfBirth date not null,
+    Address varchar(250) not null,
+    check (Gender In('M', 'F')),
+    check(SUBSTRING(ContactNumber, 1, 1) != '0'),
+    check(DateOfBirth < GETDATE())
+   
+   )
+
+
