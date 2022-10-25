556a42a537abdc1f25a522ccf26b83362d926d2d
Sujit Debnath <sujit.debnath@infosys.com>
2022-10-25 15:25:21 +0530

    NavBar Component Created

--- TravelAway/Infosys.TravelAway/TravelAwayApp/src/app/app.module.ts
+++ TravelAway/Infosys.TravelAway/TravelAwayApp/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './nabvar/nabvar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    NabvarComponent
   ],
   imports: [
     BrowserModule,
