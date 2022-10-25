ff964b2ac947f7923e0f08f3ee91e7a1eeaa1875
Venkata Krishna Siddartha Morri <venkata.morri@infosys.com>
2022-10-25 15:50:14 +0530

    login component added

--- TravelAway/Infosys.TravelAway/TravelAwayApp/src/app/app.module.ts
+++ TravelAway/Infosys.TravelAway/TravelAwayApp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NabvarComponent } from './nabvar/nabvar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
 
     AppComponent,
-    NabvarComponent
+    NabvarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
