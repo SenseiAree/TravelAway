15b344f99d1621f4a25490e3e958fc96d488ad38
Sujit Debnath <sujit.debnath@infosys.com>
2022-10-31 17:37:34 +0530

    editprofile deleted

--- TravelAway/Infosys.TravelAway/TravelAwayApp/src/app/app.module.ts
+++ TravelAway/Infosys.TravelAway/TravelAwayApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './TravelAway-Components/login/login.component';
 import { HomeComponent } from './TravelAway-Components/home/home.component';
 import { RegisterComponent } from './TravelAway-Components/register/register.component';
-import { EditProfileComponent } from './TravelAway-Components/edit-profile/edit-profile.component';
 import { FooterComponent } from './TravelAway-Components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ViewPackagesComponent } from './TravelAway-Components/view-packages/view-packages.component';
@@ -24,8 +23,6 @@ import { ViewProfileComponent } from './TravelAway-Components/view-profile/view-
     HomeComponent,
 
     RegisterComponent,
-
-    EditProfileComponent,  
 
     FooterComponent, ViewPackagesComponent, Error404PageNotFoundComponent, ViewProfileComponent,
     
